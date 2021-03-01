ebc73e7a9cf31cbb77010ea8cd699076612a0257
Margaret Kiama <maggie.m.maina@gmail.com>
2021-03-01 23:03:55 +0000

    Add files via upload

---
+++ Pthon Lists.py
@@ -0,0 +1,15 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> My ListA = (1,2,3,4,5,6)
+SyntaxError: invalid syntax
+>>> MyListA = (1,2,3,4,5,6); print(MyListA)
+(1, 2, 3, 4, 5, 6)
+>>> MyListB = list(range(1,51));
+>>> print(MyListB)
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+>>> MyListC = list(range(1,51,5));
+>>> print(MyListC)
+[1, 6, 11, 16, 21, 26, 31, 36, 41, 46]
+>>> 
+= RESTART: C:\Users\Lenovo\AppData\Local\Programs\Python\Python39\Lib\idlelib\pyshell.py
+>>> 
